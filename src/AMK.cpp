805455c5191cc42db6e908e59994509b3568a4cf
Gabry-xdvr <80488624+Gabry-xdvr@users.noreply.github.com>
2024-08-08 10:11:28 +0200

    Test motori
    Codice modificato seguendo la procedura AMK per il test dei motori

--- src/AMK.cpp
+++ src/AMK.cpp
@@ -6,7 +6,7 @@
 
 extern CANSAME5x CAN;
 extern amk_actual_values actual_values;
-
+extern int i;
 void init_device()
 {
     Serial.begin(115200);
@@ -45,7 +45,7 @@ void build_message(amk_setpoint_1 *setpoint, uint8_t *buffer)
 
 bool send_message(uint8_t *message, const int address)
 {
-    Serial.printf("Sending packet to ...0x%X\n", address);
+    //Serial.printf("Sending packet to ...0x%X\n", address);
     CAN.beginPacket(address);
     size_t bytesSent = CAN.write(message, 8);
     CAN.endPacket();
@@ -55,7 +55,7 @@ bool send_message(uint8_t *message, const int address)
 void receive_message(int packetSize)
 {
     long packet_ID = CAN.packetId();
-    Serial.printf("Received CAN packet from...0x%X\n", packet_ID);
+    //Serial.printf("Received CAN packet from...0x%X\n", packet_ID);
 
     bool is_actual_values_1 = (packet_ID == AMK_INVERTER_1_ACTUAL_VALUES_1 || packet_ID == AMK_INVERTER_2_ACTUAL_VALUES_1);
 
@@ -89,17 +89,30 @@ void process_actual_values(const amk_actual_values *actual, bool is_actual_value
 {
     if (is_actual_values_1)
     {
-        Serial.printf("AMK_Status: %d\n", actual->actual_values_1.amk_status);
+        if((actual->actual_values_1.amk_status&0x7FFF) == 0x0100) {
+            i = 1; // System ready senza errori
+        } else if((actual->actual_values_1.amk_status&0x7FFF) == 0x1100) {
+            i = 2; // DCON = 1
+        } else if((actual->actual_values_1.amk_status&0x7FFF) == 0x1900) {
+            i = 3; // QuitDCON & DCON = 1
+        } else if((actual->actual_values_1.amk_status&0x7FFF) == 0x5900) {
+            i = 4; // inverter On
+        }else if((actual->actual_values_1.amk_status&0x7FFF) == 0x7900) {
+            i = 5; // quit inverter On
+        } else {
+            i = 0; // Default case
+        }
+        Serial.printf("AMK_Status: %X\n", actual->actual_values_1.amk_status);
         Serial.printf("AMK_ActualVelocity: %d\n", actual->actual_values_1.amk_actual_velocity);
-        Serial.printf("AMK_TorqueCurrent: %f\n", (float)(ID110 * actual->actual_values_1.torque_current) / 16384);
-        Serial.printf("AMK_MagnetCurrent: %f\n", (float)(ID110 * actual->actual_values_1.magnetizing_current) / 16384);
+        //Serial.printf("AMK_TorqueCurrent: %f\n", (float)(ID110 * actual->actual_values_1.torque_current) / 16384);
+        //Serial.printf("AMK_MagnetCurrent: %f\n", (float)(ID110 * actual->actual_values_1.magnetizing_current) / 16384);
     }
     else
     {
         Serial.printf("AMK_TempMotor: %f\n", (float)actual->actual_values_2.amk_temp_motor * 0.1);
         Serial.printf("AMK_TempInverter: %f\n", (float)actual->actual_values_2.amk_temp_inverter * 0.1);
-        Serial.printf("AMK_ErrorInfo: %d\n", actual->actual_values_2.amk_error_info);
-        Serial.printf("AMK_TempIGBT: %f\n", (float)actual->actual_values_2.amk_temp_igbt * 0.1);
+        //Serial.printf("AMK_ErrorInfo: %d\n", actual->actual_values_2.amk_error_info);
+        //Serial.printf("AMK_TempIGBT: %f\n", (float)actual->actual_values_2.amk_temp_igbt * 0.1);
         
         if(actual->actual_values_2.amk_error_info != 0){
             print_error(actual->actual_values_2.amk_error_info);
@@ -111,8 +124,8 @@ void print_error(uint16_t error_code)
 {
     auto error = ERROR_MAP.find(error_code);
     if (error != ERROR_MAP.end()) {
-        std::printf("Error with code %d: %s - %s\n", error_code, error->second.first, error->second.second);
+        //std::printf("Error with code %d: %s - %s\n", error_code, error->second.first, error->second.second);
     } else {
-        std::printf("Error with code %d is unknown\n", error_code);
+        //std::printf("Error with code %d is unknown\n", error_code);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,117 @@
 #include <CANSAME5x.h>
 #include "AMK.h"
 
+
 CANSAME5x CAN;
 amk_actual_values actual_values;
 amk_setpoint_1 setpoints;
 uint8_t tx_packet[8];
 
-void set_values();
+int i, app, rpm;
+int16_t torque;
+void set_values(uint16_t control, int16_t torqueLimit, int16_t velocity);
 
 void setup()
 {
     init_device();
-
+    pinMode(A0,INPUT);
+    i=0;
     CAN.onReceive(receive_message);
 }
 
 void loop()
 {
-    set_values();
+    app = analogRead(A0);
+    Serial.println((int16_t)rpm);
+    //min 115 max 970
+    if(app>180){
+        rpm=map(app,115,970,0,6000);
+        torque = 100;
+    }
+    else
+    {
+        torque = 0;
+        rpm=0;
+    }
+    
+    switch (i)
+    {
+        case 0:
+        //caso base accensione
+        //reset degli errori setpoint = 0
+        set_values(0x0800,0,0);
+        break;
 
+        case 1:
+        //caso system ready
+        //DC ON ACK
+        set_values(0x0200,0,0);
+        break;
+
+        case 2:
+        //caso DCON=1
+        set_values(0x0200,0,0);
+        break;
+
+        case 3:
+        //caso DCON e QuitDCON=1
+        //driver enable & DC ON
+        set_values(0x0600,0,0);
+        build_message(&setpoints, tx_packet);
+        if (send_message(tx_packet, AMK_INVERTER_1_SETPOINTS_1) &&
+            send_message(tx_packet, AMK_INVERTER_2_SETPOINTS_1))
+        {
+            Serial.printf("Done Sending!\n");
+            delay(40);
+        }
+        else
+        {
+            Serial.printf("Error occurred while  sending!\n");
+        }
+        set_values(0x0700,0,0);
+        build_message(&setpoints, tx_packet);
+        if (send_message(tx_packet, AMK_INVERTER_1_SETPOINTS_1) &&
+            send_message(tx_packet, AMK_INVERTER_2_SETPOINTS_1))
+        {
+            Serial.printf("Done Sending!\n");
+            delay(40);
+        }
+        else
+        {
+            Serial.printf("Error occurred while  sending!\n");
+        } 
+        break;
+
+        case 4:
+        //caso Inverter On
+        //controller & driver enable & Dc on (setpoint = 0)
+        set_values(0x0700,0,0);  
+        break;
+
+        case 5:
+        //Caso quit Inverter On
+        //controller & driver enable & Dc on (setpoint con rotazione 10rpm coppia 2Nm)
+        set_values(0x0700,torque,(int16_t)rpm);  
+        break;
+    }
+    
     build_message(&setpoints, tx_packet);
     if (send_message(tx_packet, AMK_INVERTER_1_SETPOINTS_1) &&
         send_message(tx_packet, AMK_INVERTER_2_SETPOINTS_1))
     {
-        Serial.printf("Done Sending!\n");
+        //Serial.printf("Done Sending!\n");
+        Serial.println(i);
         delay(40);
     }
     else
     {
-        Serial.printf("Error occurred while  sending!\n");
+        //Serial.printf("Error occurred while  sending!\n");
     }
 }
 
-void set_values(){
-    setpoints.amk_control = AMK_DC_ON | AMK_DRIVER_ENABLE | AMK_INVERTER_ON | AMK_ERROR_SET_ON;
-    setpoints.amk_target_velocity = 45;
-    setpoints.amk_torque_limit_positive = 3;
-    setpoints.amk_torque_limit_negative = -3;
+void set_values(uint16_t control, int16_t torqueLimit, int16_t velocity){
+    setpoints.amk_control = control;
+    setpoints.amk_target_velocity = velocity;
+    setpoints.amk_torque_limit_positive = torqueLimit;
+    setpoints.amk_torque_limit_negative = 0;
 }
